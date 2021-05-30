4e87cbd31e984d3d73132fd4f981fc8cc687612d
Oleksandr Leiberiuk <Lejberyk.ua@gmail.com>
2021-05-30 14:23:45 +0300

    change js, add style, pnofity

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import debounce from 'lodash.debounce';
 import '@pnotify/core/dist/BrightTheme.css';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/mobile/dist/PNotifyMobile.css';
-import { error } from '@pnotify/core/dist/PNotify.js';
+import { alert, error } from '@pnotify/core/dist/PNotify.js';
 
 const refs = {
   cardContainer: document.querySelector('.js-card'),
@@ -25,22 +25,31 @@ function inputEvent(e) {
 function renderCountryCard(countries) {
   clearCardContainer();
 
-  if (countries.length === 1) {
-    const markup = cardOneCountry(countries[0]);
-    refs.cardContainer.innerHTML = markup;
-    // refs.cardContainer.insertAdjacentHTML('beforeend', markup);
-  } else if (countries.length > 1 && countries.length <= 10) {
+  if (countries.length > 1 && countries.length <= 10) {
     const markup = cardListCountries(countries);
-    // refs.cardContainer.insertAdjacentHTML('beforeend', markup);
-    refs.cardContainer.innerHTML = markup;
+    return (refs.cardContainer.innerHTML = markup);
+  } else if (countries.length === 1) {
+    const markup = cardOneCountry(countries[0]);
+    return (refs.cardContainer.innerHTML = markup);
   } else if (countries.length > 10) {
-    error({
+    return error({
       title: 'Oh No!',
       text: 'Something terrible happened.',
+    });
+  } else if (countries.status === 404) {
+    return error({
+      title: 'Oh sorry, error 404!!!',
+      text: 'Your query returned an incorrect result(((',
     });
   }
 
   function clearCardContainer() {
     refs.cardContainer.innerHTML = '';
   }
+}
+
+function onFetchError() {
+  alert({
+    text: 'Caution, not correct data !!!',
+  });
 }
